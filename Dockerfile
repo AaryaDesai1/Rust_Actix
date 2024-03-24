820c75fb4f453847430da4480462f813e41d87e9
AaryaDesai1 <aad64@duke.edu>
2024-03-24 19:05:27 -0400

    Adding changes to project

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,16 @@
-# Use the official Rust image as the base image
+# Multi-stage build to reduce the size of the final Docker image
 FROM rust:latest as builder
 
-# Set the working directory in the container
 WORKDIR /usr/src/app
-
-# Copy the source code into the container
 COPY . .
 
-# Build the Rust application
 RUN cargo build --release
 
-# Use a smaller base image for the final container
 FROM debian:buster-slim
 
-# Set the working directory in the container
 WORKDIR /usr/src/app
-
-# Copy the compiled binary from the builder stage to the final image
 COPY --from=builder /usr/src/app/target/release/rust_actix .
 
-# Expose the port on which your Actix web service listens
 EXPOSE 8080
 
-# Command to run the binary when the container starts
 CMD ["./rust_actix"]
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{post, App, HttpServer, HttpResponse, web};
+use actix_web::Responder;
+use serde::Deserialize;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+const USERNAME: &str = "admin";
+const PASSWORD: &str = "password";
+
+#[post("/login")]
+async fn login(form: web::Form<LoginForm>) -> impl Responder {
+    if form.username == USERNAME && form.password == PASSWORD {
+        HttpResponse::Ok().body("Login successful!")
+    } else {
+        HttpResponse::Unauthorized().body("Invalid username or password")
+    }
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[derive(Deserialize)]
+struct LoginForm {
+    username: String,
+    password: String,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(login)
+            .service(actix_files::Files::new("/", "src/static").index_file("login.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
